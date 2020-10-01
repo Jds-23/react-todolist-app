f38d6894be4c1498217130b94469d958f36c46ae
jds <joydeepsingha68@gamil.com>
2020-10-01 11:11:12 +0530

    Initialization

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./TodoList";
+import "./index.css";
+ReactDOM.render(
+    <div className="container">
+        <TodoList/>
+    </div>,
+        document.getElementById('root')
+);
